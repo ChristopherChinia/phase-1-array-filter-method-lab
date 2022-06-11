5ab0a7ea1592972862abb7bc0a20805b3bcc0f8b
christopherchinia <chinia214@gmail.com>
2022-06-11 09:18:34 -0400

    commit tree 71e45baf7632252c636555ecfd6af1fde1eb6b97

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, values) {
+  return drivers.filter(
+    (personName) => personName.toLowerCase() === values.toLowerCase()
+  );
+}
+function fuzzyMatch(drivers, values) {
+  return drivers.filter((personName) => personName.startsWith(values));
+}
+function matchName(drivers, values){
+    return drivers.filter(personName => personName.name === values)
+}
